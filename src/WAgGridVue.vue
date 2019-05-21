87d594b4afca9006cc5cad06a17d437633548b8b
yuda-lyu <firsemisphere@gmail.com>
2019-05-21 20:57:09 +0800

    add test, add jsdoc, modify jsdoc format

--- src/WAgGridVue.vue
+++ src/WAgGridVue.vue
@@ -45,14 +45,40 @@ import onTooltip from 'wsemi/src/onTooltip.mjs'
 //add
 window.ttWAgGridVue = onTooltip
 
-let opt = {
+/**
+ * @vue-prop {Object} opt 輸入資料設定物件
+ * @vue-prop {Array} opt.keys 輸入資料各欄位keys
+ * @vue-prop {Array} opt.rows 輸入資料列，各列為物件，內含各欄位keys之值，例[{},{},...,{}]
+ * @vue-prop {Object} [opt.kpHead={}] 輸入key對應head物件，預設為keys
+ * @vue-prop {Object} [opt.kpHeadTooltip={}] 輸入key對應需tooltip的html字串物件，於各head處滑鼠移入時觸發，預設為{}
+ * @vue-prop {Object} [opt.kpHeadAlighH={}] 輸入key對應head之左右對齊字串物件，預設為各key值為center
+ * @vue-prop {Object} [opt.kpHeadSort={}] 輸入key對應head之是否允許排序物件，預設為各key值為true
+ * @vue-prop {Object} [opt.kpHeadFix={}] 輸入key對應head之是否固定於左側物件，預設為各key值為false
+ * @vue-prop {Object} [opt.kpHeadFilter={}] 輸入key對應head之是否允許過濾物件，預設為各key值為true
+ * @vue-prop {Object} [opt.kpRowStyle={}] 輸入key對應row style之物件，可設定key欄之cell值所需對應之row style，預設為{}
+ * @vue-prop {Object} [opt.defCellMinWidth={}] 輸入cell預設最小寬度數字，預設為null
+ * @vue-prop {Object} [opt.kpRowDrag={}] [專有:ag-grid] 輸入key對應row之是否能拖曳排序物件，預設為各key值為false
+ * @vue-prop {Object} [opt.kpCellWidth={}] 輸入key對應cell之寬度物件，預設為各key值為''
+ * @vue-prop {Object} [opt.kpCellRender={}] 輸入key對應cell之渲染函數物件，預設為{}
+ * @vue-prop {Object} [opt.kpCellTooltip={}] 輸入key對應cell之tooltip的html字串物件，於各cell處滑鼠移入時觸發，預設為{}
+ * @vue-prop {Object} [opt.kpCellAlighH={}] 輸入key對應cell之左右對齊字串物件，預設為各key值為center
+ * @vue-prop {Object} [opt.kpCellEditable={}] 輸入key對應cell之是否可編輯物件，預設為各key值為false
+ * @vue-prop {Function} [opt.rowClick={}] 輸入row click之觸發事件，預設為function(){}
+ * @vue-prop {Function} [opt.rowDbClick={}] 輸入row double click之觸發事件，預設為function(){}
+ * @vue-prop {Function} [opt.rowChange={}] 輸入row change之觸發事件，預設為function(){}
+ * @vue-prop {Function} [opt.cellClick={}] 輸入cell click之觸發事件，預設為function(){}
+ * @vue-prop {Function} [opt.cellDbClick={}] 輸入cell double click之觸發事件，預設為function(){}
+ * @vue-prop {Function} [opt.cellChange={}] 輸入cell change之觸發事件，預設為function(){}
+ * @vue-prop {Number} [tbheight=300] 表格高度，預設300px
+ * @vue-prop {String} [search=''] 輸入過濾字串，預設為''
+ * @vue-event {Null} refresh 刷新表格，無輸入與回傳
+ * @vue-event {Array} showKeys 輸入要顯示欄位的keys，數量最多為原本初始化的keys，可更改順序，無回傳
+ * @vue-event {Null} getDisplayData 無輸入，會回傳目前表格所顯示之數據
+ */
+export default {
     props: {
         opt: {
             type: Object,
-        },
-        loading: {
-            type: Boolean,
-            default: false
         },
         tbheight: {
             type: Number,
@@ -673,7 +699,6 @@ let opt = {
 
     },
 }
-export default opt
 </script>
 
 <style>
